d74653f37541168bcd0b8ca4f5c80329717ce819
WellingtonSantos1997 <wellingtonsantos1997@outlook.com>
2025-03-16 10:03:33 -0300

    C

---
+++ OlaMundo/olamundo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char nome[20];
+    int idade;
+
+    printf("Digite seu nome: ");
+    scanf("%s",&nome);
+    printf("Nome: %s \n",nome);
+    printf("Digite sua idade: ");
+    scanf("  %d",&idade);
+    printf("Idade: %d",idade);
+
+
+
+}
